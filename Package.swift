83811465f5a0b9605cf7ed9200aa2eae0120ed3b
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2025-06-26 11:30:07 +0700

    Package.swift updated.

--- Package.swift
+++ Package.swift
@@ -1,3 +1,25 @@
+// swift-tools-version:6.1
+
 import PackageDescription
 
-let package = Package(name: "NSKJSON")
+let package = Package(
+    name: "NSKJSON",
+    platforms: [
+        .macOS(.v11_0),
+        .iOS(.v15),
+        .tvOS(.v12),
+        .watchOS(.v4),
+        .visionOS(.v1)
+    ],
+    products: [
+        .library(name: "NSKJSON",
+                 targets: ["NSKJSON"]),
+        .library(name: "NSKJSON-Dynamic",
+                 type: .dynamic,
+                 targets: ["NSKJSON"]),
+    ],
+    targets: [
+        .target(name: "NSKJSON",
+                path: "Source/Swift"),
+    ]
+)
